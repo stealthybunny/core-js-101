6684f9d290b2bfd27bcf2eab4ddbb34dae245e5b
Peter Zaichyk <zaichikpetr@gmail.com>
2022-11-28 16:33:04 +0300

    feat: task 03 - 24 tsks from 32 are ready

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -554,7 +554,7 @@ function getIntervalArray(start, end) {
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
 function distinct(/* arr */) {
-  throw new Error('Not implemented'); 
+  throw new Error('Not implemented');
 }
 
 /**
