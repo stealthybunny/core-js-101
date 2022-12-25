5bbb85e94d2bdbd5ba32c21d35b1f61296f2e475
Peter Zaichyk <zaichikpetr@gmail.com>
2022-12-25 16:25:38 +0300

    task 06: 3/4

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -59,8 +59,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -82,8 +82,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
